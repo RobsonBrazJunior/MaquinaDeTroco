d688988366288733ea475b0ebb5a53362abba267
Robson <robsonbrazjunior@gmail.com>
2022-09-15 16:23:24 -0300

    Create basic MaquinaDeTrocoContext

---
+++ src/MaquinaDeTroco.Infra.Data/Context/MaquinaDeTrocoContext.cs
@@ -0,0 +1,12 @@
+﻿using MaquinaDeTroco.Domain.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace MaquinaDeTroco.Infra.Data.Context
+{
+	public sealed class MaquinaDeTrocoContext : DbContext
+	{
+		public DbSet<User> Users { get; set; }
+
+		public MaquinaDeTrocoContext(DbContextOptions<MaquinaDeTrocoContext> options) : base(options) { }
+	}
+}
