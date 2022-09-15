aaca604148d997088fed8da3da1d5510b468e52d
Robson <robsonbrazjunior@gmail.com>
2022-09-15 15:56:35 -0300

    Create User model

---
+++ src/MaquinaDeTroco.Domain/Models/User.cs
@@ -0,0 +1,10 @@
+﻿namespace MaquinaDeTroco.Domain.Models
+{
+	public class User
+	{
+		public Guid Id { get; private set; }
+		public string? Name { get; private set; }
+		public string? Password { get; private set; }
+		public string? Email { get; private set; }
+	}
+}
