cd689da954532e055af044b0420fa03abcb220de
Robson <robsonbrazjunior@gmail.com>
2022-09-15 16:59:40 -0300

    Create UserRepository class

---
+++ src/MaquinaDeTroco.Infra.Data/Repository/Repositories/UserRepository.cs
@@ -0,0 +1,13 @@
+﻿using MaquinaDeTroco.Domain.Models;
+using MaquinaDeTroco.Infra.Data.Context;
+using MaquinaDeTroco.Infra.Data.Repository.Interfaces;
+
+namespace MaquinaDeTroco.Infra.Data.Repository.Repositories
+{
+	public class UserRepository : GenericRepository<User>, IUserRepository
+	{
+		public UserRepository(MaquinaDeTrocoContext context) : base(context)
+		{
+		}
+	}
+}
