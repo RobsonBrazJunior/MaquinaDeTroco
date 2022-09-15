e970bca88b9fe5bda3d56b3e068ff3b268e05b69
Robson <robsonbrazjunior@gmail.com>
2022-09-15 17:15:56 -0300

    Configuring UniOfWork pattern

---
+++ src/MaquinaDeTroco.Infra.Data/Repository/Interfaces/IUnitOfWork.cs
@@ -0,0 +1,8 @@
+﻿namespace MaquinaDeTroco.Infra.Data.Repository.Interfaces
+{
+	public interface IUnitOfWork : IDisposable
+	{
+		IUserRepository User { get; }
+		int Save();
+	}
+}
---
+++ src/MaquinaDeTroco.Infra.Data/Repository/Repositories/UnitOfWork.cs
@@ -0,0 +1,34 @@
+﻿using MaquinaDeTroco.Infra.Data.Context;
+using MaquinaDeTroco.Infra.Data.Repository.Interfaces;
+
+namespace MaquinaDeTroco.Infra.Data.Repository.Repositories
+{
+	public class UnitOfWork : IUnitOfWork
+	{
+		#region Fields
+		private readonly MaquinaDeTrocoContext _context;
+		private readonly IUserRepository _user;
+		#endregion
+
+		#region Properties
+		public IUserRepository User => _user;
+		#endregion
+
+		public UnitOfWork(MaquinaDeTrocoContext context)
+		{
+			_context = context;
+			_user = new UserRepository(context);
+		}
+
+		public int Save()
+		{
+			return _context.SaveChanges();
+		}
+
+		public void Dispose()
+		{
+			_context.Dispose();
+			GC.SuppressFinalize(this);
+		}
+	}
+}
