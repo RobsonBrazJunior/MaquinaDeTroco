d07ef5af55a30683fe4ada0b6428af1bdcb8f741
Robson <robsonbrazjunior@gmail.com>
2022-09-15 19:26:25 -0300

    Adding Dependency Injection for UnitOfWork and UserService on Program class

--- src/MaquinaDeTroco.Application.Api/Program.cs
+++ src/MaquinaDeTroco.Application.Api/Program.cs
@@ -1,4 +1,8 @@
 using MaquinaDeTroco.Infra.Data.Context;
+using MaquinaDeTroco.Infra.Data.Repository.Interfaces;
+using MaquinaDeTroco.Infra.Data.Repository.Repositories;
+using MaquinaDeTroco.Services.Interfaces;
+using MaquinaDeTroco.Services.Services;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -17,6 +21,8 @@ builder.Services.AddDbContext<MaquinaDeTrocoContext>(options =>
 	options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"),
 		p => p.EnableRetryOnFailure(2, TimeSpan.FromSeconds(5), null).MigrationsHistoryTable("_version_migration"));
 });
+builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
+builder.Services.AddScoped<IUserService, UserService>();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
