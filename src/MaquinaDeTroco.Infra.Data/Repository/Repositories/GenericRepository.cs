081bc0c27c035b0c7f51ff52c5bdf2a240337f22
Robson <robsonbrazjunior@gmail.com>
2022-09-15 16:55:13 -0300

    Create GenericRepository class

---
+++ src/MaquinaDeTroco.Infra.Data/Repository/Repositories/GenericRepository.cs
@@ -0,0 +1,57 @@
+﻿using MaquinaDeTroco.Infra.Data.Context;
+using MaquinaDeTroco.Infra.Data.Repository.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
+
+namespace MaquinaDeTroco.Infra.Data.Repository.Repositories
+{
+	public class GenericRepository<T> : IGenericRepository<T> where T : class
+	{
+		private readonly MaquinaDeTrocoContext _context;
+
+		public GenericRepository(MaquinaDeTrocoContext context)
+		{
+			_context = context;
+		}
+
+		public void Add(T entity)
+		{
+			_context.Set<T>().Add(entity);
+		}
+		public void Update(T entity)
+		{
+			_context.Set<T>().Attach(entity);
+			_context.Entry(entity).State = EntityState.Modified;
+		}
+
+		public void AddRange(IEnumerable<T> entities)
+		{
+			_context.Set<T>().AddRange(entities);
+		}
+
+		public IEnumerable<T> Find(Expression<Func<T, bool>> expression)
+		{
+			return _context.Set<T>().Where(expression);
+		}
+
+		public IEnumerable<T> GetAll()
+		{
+			return _context.Set<T>().ToList();
+		}
+
+		public T GetById(int id)
+		{
+			return _context.Set<T>().Find(id);
+		}
+
+		public void Remove(T entity)
+		{
+			_context.Set<T>().Remove(entity);
+		}
+
+		public void RemoveRange(IEnumerable<T> entities)
+		{
+			_context.Set<T>().RemoveRange(entities);
+		}
+	}
+}
