0668892eb1a8a7ef5a3347c50b2208efc6be1e5f
Robson <robsonbrazjunior@gmail.com>
2022-09-15 18:17:17 -0300

    Create Services for User

---
+++ src/MaquinaDeTroco.Services/Interfaces/IUserService.cs
@@ -0,0 +1,13 @@
+﻿using MaquinaDeTroco.Domain.Models;
+
+namespace MaquinaDeTroco.Services.Interfaces
+{
+	public interface IUserService : IDisposable
+	{
+		User GetById(Guid id);
+		IEnumerable<User> GetAll();
+		void Add(User entity);
+		void Update(User entity);
+		void Remove(User entity);
+	}
+}
---
+++ src/MaquinaDeTroco.Services/Services/UserService.cs
@@ -0,0 +1,41 @@
+﻿using MaquinaDeTroco.Domain.Models;
+using MaquinaDeTroco.Infra.Data.Repository.Interfaces;
+using MaquinaDeTroco.Services.Interfaces;
+
+namespace MaquinaDeTroco.Services.Services
+{
+	public class UserService : IUserService
+	{
+		private readonly IUnitOfWork _unitOfWork;
+
+		public UserService(IUnitOfWork unitOfWork)
+		{
+			_unitOfWork = unitOfWork;
+		}
+		public User GetById(Guid id) => _unitOfWork.User.GetById(id);
+
+		public IEnumerable<User> GetAll() => _unitOfWork.User.GetAll();
+
+		public void Add(User entity)
+		{
+			_unitOfWork.User.Add(entity);
+			_unitOfWork.Save();
+		}
+		public void Update(User entity)
+		{
+			_unitOfWork.User.Update(entity);
+			_unitOfWork.Save();
+		}
+
+		public void Remove(User entity)
+		{
+			_unitOfWork.User.Remove(entity);
+			_unitOfWork.Save();
+		}
+
+		public void Dispose()
+		{
+			GC.SuppressFinalize(this);
+		}
+	}
+}
