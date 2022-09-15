39875172b21b563ad3d45c468b23290ac17bf4ef
Robson <robsonbrazjunior@gmail.com>
2022-09-15 19:07:59 -0300

    Adding DbContext in Dependency Injection on Program class

--- src/MaquinaDeTroco.Application.Api/Program.cs
+++ src/MaquinaDeTroco.Application.Api/Program.cs
@@ -1,8 +1,22 @@
+using MaquinaDeTroco.Infra.Data.Context;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Configuration
+	.SetBasePath(builder.Environment.ContentRootPath)
+	.AddJsonFile("appsettings.json", true, true)
+	.AddJsonFile($"appsettings.{builder.Environment.EnvironmentName}.json", true, true)
+	.AddEnvironmentVariables();
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddDbContext<MaquinaDeTrocoContext>(options =>
+{
+	options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"),
+		p => p.EnableRetryOnFailure(2, TimeSpan.FromSeconds(5), null).MigrationsHistoryTable("_version_migration"));
+});
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
