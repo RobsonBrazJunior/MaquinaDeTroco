6996f69464bf6b4b50675d2d321fb93751a5f532
Robson <robsonbrazjunior@gmail.com>
2022-09-16 11:33:00 -0300

    Create action Get for UserController

---
+++ src/MaquinaDeTroco.Application.Api/Controllers/UserController.cs
@@ -0,0 +1,32 @@
+﻿using MaquinaDeTroco.Services.Interfaces;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MaquinaDeTroco.Application.Api.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class UserController : ControllerBase
+	{
+		private readonly IUserService _userService;
+
+		public UserController(IUserService userService)
+		{
+			_userService = userService;
+		}
+
+		[HttpGet("Get/{id:Guid}")]
+		public IActionResult Get(Guid id)
+		{
+			if (id == Guid.Empty)
+				return BadRequest();
+
+			var result = _userService.GetById(id);
+
+			if (result == null)
+				return NotFound();
+
+			return Ok(result);
+		}
+	}
+}
